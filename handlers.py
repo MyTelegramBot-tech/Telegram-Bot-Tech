6c3fff14de206ca4d48e395f26c9945c8323fdd5
MyTelegramBot-tech <mitiai84@gmail.com>
2025-04-03 16:48:08 +0800

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,5 +1,7 @@
 # handlers.py
-from bot import bot
+import telebot
+
+bot = telebot.TeleBot("7578950871:AAGfv2zmRr6x532S0H4zJ2h2k95c_PNKSB4")
 
 @bot.message_handler(commands=['start'])
 def start(message):
